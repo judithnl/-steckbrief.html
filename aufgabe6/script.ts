dc8feca0b460ee21fc384c2457f2f7cd78cedbf4
judithnl <92431648+judithnl@users.noreply.github.com>
2021-11-26 17:00:04 +0100

    aufgabe6

---
+++ aufgabe6/script.ts
@@ -0,0 +1,61 @@
+var eu8 = 1028;
+var eu18 = 1235.5;
+var eu:string = "pa";
+
+var na8 = 6600.4;
+var na18 = 6035.6;
+var na:string="Nordamerika";
+
+var sa8 = 1132.6;
+var sa18 = 1261.5;
+var sa:string ="Südamerika";
+
+var af8 = 1028;
+var af18 = 1235.5;
+var af:string= "Afrika";
+
+var as8 = 12954.7;
+var as18 = 16274.1;
+var as:string = "Asien";
+    
+var aus8 = 1993;
+var aus18 = 2100.5;
+var aus = "Australien"
+
+var gesamt8 = eu8+na8+sa8+af8+as8+aus8;
+var gesamt18 = eu18+na18+sa18+af18+as18+aus18;
+
+namespace el{
+      window.addEventListener("load", function(){
+
+        document.querySelector(".africa").addEventListener("click", function(){
+            what(af, af18, af8);
+        });
+        document.querySelector(".asia").addEventListener("click", function(){
+            what(as, as18, as8);
+        });
+        document.querySelector(".australia").addEventListener("click", function(){
+            what(aus, aus18, aus8);
+        });
+        document.querySelector(".northamerica").addEventListener("click", function(){
+            what(na, na18, na8);
+        });
+        document.querySelector(".southamerica").addEventListener("click", function(){
+            what(sa, sa18, sa8);
+        });
+   
+});
+
+   
+   
+
+
+function what(conti, conti18:number, conti8:number) {
+
+document.querySelector("#titleRegion").innerHTML=conti;
+document.querySelector("#co2").innerHTML=conti18.toFixed(1);
+document.querySelector("#prozent").innerHTML= (((conti18 - conti8) / conti8) * 100).toFixed(1);
+document.querySelector("#abso").innerHTML=(conti18-conti8).toFixed(1);
+};
+
+ };
