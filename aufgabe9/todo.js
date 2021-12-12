d95e03d138ec9680d6ebea6d2286a47cc547304b
judithnl <92431648+judithnl@users.noreply.github.com>
2021-12-12 22:18:38 +0100

    aufgabe9

--- aufgabe9/todo.js
+++ aufgabe9/todo.js
@@ -1,2 +1,46 @@
-document.querySelector(".add").addEventListener;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+window.addEventListener("load", function () {
+    var index = 0;
+    var text = document.createElement("p");
+    var input = document.querySelector("#input");
+    var add = document.querySelector("#add");
+    var trash = document.querySelector("#trash");
+    var done = document.createElement("div");
+    var haken = document.createElement("button");
+    var trashbutton = document.createElement("button");
+    //funktion zähler
+    function count() {
+        document.querySelector("#gesamt").innerHTML = String(index);
+    }
+    //zähler
+    add.addEventListener("click", function () {
+        index++;
+        count();
+        //eingabefeld
+        text.classList.add("text");
+        text.innerText = input.value;
+        trash.appendChild(text);
+        //done
+        done.setAttribute("class", "fas fa-circle");
+        trash.appendChild(done);
+        haken.setAttribute("class", "fas fa-check");
+        trash.appendChild(haken);
+        //löschen
+        trashbutton.addEventListener("click", function () {
+            trash.removeChild(text);
+            trash.removeChild(done);
+            trash.removeChild(haken);
+            trash.removeChild(trashbutton);
+            index--;
+            count();
+        });
+        //abhaken
+        haken.addEventListener("click", function () {
+            if (haken.style.opacity != "100%") {
+                haken.style.opacity = "100%";
+            }
+        });
+    });
+});
 //# sourceMappingURL=todo.js.map
--- aufgabe9/todo.ts
+++ aufgabe9/todo.ts
@@ -1,1 +1,60 @@
-document.querySelector(".add").addEventListener
+import { count, countReset } from "console";
+
+window.addEventListener("load",function(){
+let index:number=0;
+
+let text=document.createElement("p");
+let input:HTMLInputElement=document.querySelector("#input");
+let add=document.querySelector("#add");
+let trash=document.querySelector("#trash");
+
+let done=document.createElement("div");
+let haken=document.createElement("button");
+
+let trashbutton=document.createElement("button");
+
+//funktion zähler
+function count(){
+    document.querySelector("#gesamt").innerHTML=String(index);
+}
+
+//zähler
+add.addEventListener("click", function(){
+    index++
+    count()
+
+
+//eingabefeld
+text.classList.add("text");
+text.innerText=input.value;
+trash.appendChild(text);
+
+//done
+
+done.setAttribute("class", "fas fa-circle");
+trash.appendChild(done);
+
+haken.setAttribute("class", "fas fa-check");
+trash.appendChild(haken);
+
+//löschen
+trashbutton.addEventListener("click",function(){
+    trash.removeChild(text);
+    trash.removeChild(done);
+    trash.removeChild(haken);
+    trash.removeChild(trashbutton);
+
+    index--;
+    count();
+})
+//abhaken
+haken.addEventListener("click",function(){
+    if(haken.style.opacity !="100%"){
+        haken.style.opacity="100%";
+    }
+
+
+})
+})
+
+});
